4c8f885064251f767985bf648bdb7b13a7db6d34
小姐请留步 <280417314@qq.com>
2018-10-17 21:15:54 +0800

    - #0:Reconstruction project

--- Sample/SAE.ShoppingMall/src/SAE.ShoppingMall.Admin/wwwroot/content/js/base.js
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="../../lib/requirejs/require.js" />
-/// <reference path="../../lib/knockout/dist/knockout.js" />
-/// <reference path="../../lib/jquery/dist/jquery.min.js" />
-define(["bootstrap", "adminlte"]);
--- Sample/SAE.ShoppingMall/src/SAE.ShoppingMall.Admin/wwwroot/content/js/bind.js
+++
@@ -1,4 +0,0 @@
-﻿define(["ko", "vm"], function (ko, vm) {
-    //初始化绑定对象
-    ko.applyBindings(vm);
-});
